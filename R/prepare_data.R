d31bd1cb2e840ebc3a5f5702de9766ed24a2a1dc
jeremieboudreault <59902559+jeremieboudreault@users.noreply.github.com>
2021-04-05 13:32:08 -0400

    Major update to prepare data : - All lanes segments are now plotted intead of an aggregation of all. A bit slower, but more flexible for now. - Added a better viewing on the improvment to the cycling network and to the road construction. - Added the "winter" network to the map.

--- R/prepare_data.R
+++ R/prepare_data.R
@@ -1,10 +1,10 @@
 # prepare_data.R
 
 
-# Project : map_reseau_cyclable_qc
-# Author  : Jeremie Boudreault 
+# Project : carte_reseau_cyclable_qc
+# Author  : Jeremie Boudreault
 # Email   : JeremieBoudreault11@gmail.com
-# Depends : R 3.6.3
+# Depends : R (v3.6.3)
 # License : ---
 
 
@@ -23,10 +23,12 @@ library(sp)
 
 # Define a list with the correct names and IDS.
 map_names <- list(
+    RCE    = "Réseau cyclable estival",
     PC     = "Piste cyclable",
     BC     = "Bande cyclable",
     CD     = "Chaussée désignée",
     FUTUR  = "Améliorations 2021",
+    WINTER = "Réseau 4 saisons",
     RW     = "Travaux routiers"
 )
 
@@ -36,11 +38,11 @@ map_names <- list(
 
 # Quebec city cycle network.
 lines_qc <- rgdal::readOGR(
-  dsn              = file.path("data", "reseau_qc"),
-  layer            = "vdq-reseaucyclable",
-  use_iconv        = TRUE,
-  encoding         = "UTF-8",
-  stringsAsFactors = FALSE
+    dsn              = file.path("data", "reseau_qc"),
+    layer            = "vdq-reseaucyclable",
+    use_iconv        = TRUE,
+    encoding         = "UTF-8",
+    stringsAsFactors = FALSE
 )
 
 # Quebec city road work.
@@ -70,39 +72,57 @@ table(lines_qc@data$TYPE, useNA = "always")
 
 # Replaces NAs by "N".
 lines_qc$FUTUR[is.na(lines_qc$FUTUR)]   <-  "N"
+lines_qc$WINTER[is.na(lines_qc$WINTER)] <-  "N"
 
 
-# Split into multiple polygons -------------------------------------------------
+# Add characteristics ----------------------------------------------------------
 
 
-# Pistes cyclables (PC).
-lines_pc <- rgeos::gLineMerge(lines_qc[
-    lines_qc$TYPE   == map_names[["PC"]] &
-    lines_qc$FUTUR  != "Y"
-, ])
+# Adjust size of the line.
+lines_qc$LWD <- 0
+lines_qc$LWD[lines_qc$TYPE == map_names["PC"]] <- 3
+lines_qc$LWD[lines_qc$TYPE == map_names["BC"]] <- 2.5
+lines_qc$LWD[lines_qc$TYPE == map_names["CD"]] <- 2.5
 
-# Bandes cyclablaes (BC).
-lines_bc <- rgeos::gLineMerge(lines_qc[
-    lines_qc$TYPE   == map_names[["BC"]] &
-    lines_qc$FUTUR  != "Y"
-, ])
 
-# Chaussée désignée (CD).
-lines_cd <- rgeos::gLineMerge(lines_qc[
-    lines_qc$TYPE   == map_names[["CD"]] &
-    lines_qc$FUTUR  != "Y"
-, ])
+# Adjust color.
+lines_qc$COL <- ""
+lines_qc$COL[lines_qc$TYPE == map_names["PC"]] <- "darkgreen"
+lines_qc$COL[lines_qc$TYPE == map_names["BC"]] <- "green"
+lines_qc$COL[lines_qc$TYPE == map_names["CD"]] <- "green"
+
+
+# Extract <WINTER> and <FUTUR> -------------------------------------------------
+
+
+# Réseau quatre saisons.
+lines_winter <- lines_qc[lines_qc$WINTER == "Y", ]
 
 # Amélioration au réseau.
 lines_futur <- lines_qc[lines_qc$FUTUR == "Y", ]
 
+# Remove amélioration au réseau.
+lines_qc <- lines_qc[lines_qc$FUTUR != "Y", ]
 
-# Define palette for Road Work -------------------------------------------------
+
+# Create icons for road work ---------------------------------------------------
 
 
-pal_rw <- leaflet::colorFactor(
-    palette = c("red", "yellow"),
-    domain  = c("WARNING", "CLOSED")
+icons_rw <- leaflet::iconList(
+    CLOSED  = leaflet::makeIcon(
+        iconUrl     = file.path("www", "closed.png"),
+        iconWidth   = 20L,
+        iconHeight  = 20L,
+        iconAnchorX = 8L,
+        iconAnchorY = 8L
+    ),
+    WARNING = leaflet::makeIcon(
+        iconUrl      = file.path("www", "warning.png"),
+        iconWidth   = 20L,
+        iconHeight  = 20L,
+        iconAnchorX = 10L,
+        iconAnchorY = 10L
+    )
 )
 
 
@@ -110,84 +130,69 @@ pal_rw <- leaflet::colorFactor(
 
 
 # Create the base map.
-leaflet::leaflet() %>%
-    
+map <- leaflet::leaflet() %>%
+
 # Add a tile.
 leaflet::addProviderTiles(provider = leaflet::providers$CartoDB.Voyager) %>%
-    
-# Add "Pistes Cyclables".
+
+# Add "Reseau cyclable estival".
 leaflet::addPolylines(
-    data         = lines_pc,
-    layerId      = map_names[["PC"]],
-    group        = map_names[["PC"]],
+    data         = lines_qc,
+    group        = map_names[["RCE"]],
     stroke       = TRUE,
-    color        = "darkgreen",
-    weight       = 3L,
+    color        = ~COL,
+    weight       = ~LWD,
     opacity      = 1L,
     dashArray    = NULL,
     smoothFactor = 1
 ) %>%
 
-# Add the "Bandes cyclables".
-leaflet::addPolylines(
-    data         = lines_bc,
-    layerId      = map_names[["BC"]],
-    group        = map_names[["BC"]],
-    stroke       = TRUE,
-    color        = "green",
-    weight       = 3L,
-    opacity      = 1L,
-    dashArray    = NULL,
-    smoothFactor = 1
-) %>%
-
-# Add the "Chaussée Désignée".
-leaflet::addPolylines(
-    data         = lines_cd,
-    group        = map_names[["CD"]],
-    stroke       = TRUE,
-    color        = "green",
-    weight       = 2.5,
-    opacity      = 0.95,
-    dashArray    = NULL,
-    smoothFactor = 1
-) %>%
-    
 # Add the "Améliorations".
 leaflet::addPolylines(
     data         = lines_futur,
     group        = map_names[["FUTUR"]],
     stroke       = TRUE,
-    color        = "#2E8DEC",
-    weight       = 2.5,
+    color        = "#EB5E28",
+    weight       = ~LWD,
     opacity      = 0.95,
     dashArray    = NULL,
     smoothFactor = 1,
+    label        = ~NOM_TOPOGR,
+    labelOptions = leaflet::labelOptions(noHide = TRUE),
     popup        = ~DESC
 ) %>%
-    
+
+# Add the "Winter".
+leaflet::addPolylines(
+    data         = lines_winter,
+    group        = map_names[["WINTER"]],
+    stroke       = TRUE,
+    color        = "#2E8DEC",
+    weight       = ~LWD,
+    opacity      = 0.95,
+    dashArray    = NULL,
+    smoothFactor = 1
+) %>%
+
 # Add the "Travaux".
-leaflet::addCircleMarkers(
+leaflet::addMarkers(
     data        = road_work,
     group       = map_names[["RW"]],
-    radius      = 5L,
-    stroke      = TRUE,
-    weight      = 2,
-    color       = "black",
-    opacity     = 1L,
-    fillColor   = ~pal_rw(TYPE),
-    fillOpacity = 1L,
+    icon        = ~icons_rw[TYPE],
     popup       = ~DESC
 ) %>%
-    
+
 # Hide groups.
 leaflet::hideGroup(
-    group = c(map_names[["FUTUR"]], map_names[["RW"]])
+    group = unlist(map_names[c("FUTUR", "RW", "WINTER")], use.names = FALSE)
 ) %>%
 
 # Add controls.
 leaflet::addLayersControl(
-    overlayGroups = unlist(map_names, use.names = FALSE),
+    overlayGroups = unlist(
+        x         = map_names[c("RCE", "WINTER", "FUTUR", "RW")],
+        use.names = FALSE
+    ),
     options       = layersControlOptions(collapse = FALSE)
 )
-    
+
